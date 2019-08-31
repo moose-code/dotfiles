efee160a4491dd7b2a9768727a029e63e97a32bb
Jason Smythe <jason@smythemail.za.net>
2019-08-31 16:42:43 +0200

    Add zshrc file

---
+++ zsh/setupZshLinks.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ln -s ${PWD}/zlogin ~/.zlogin
+ln -s ${PWD}/zlogout ~/.zlogout
+ln -s ${PWD}/zpreztorc ~/.zpreztorc
+ln -s ${PWD}/zprofile ~/.zprofile
+ln -s ${PWD}/zshenv ~/.zshenv
+ln -s ${PWD}/zshrc ~/.zshrc
